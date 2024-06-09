a12f04853bf49bd9d29307623fa48381bfc50f54
Yogesh@7072 <yogeshwarcha2421@gmail.com>
2024-06-09 18:55:42 +0530

    Your commit message

---
+++ src/main/java/com/project/vehical/VehicalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.vehical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicalServiceApplication.class, args);
+	}
+
+}
